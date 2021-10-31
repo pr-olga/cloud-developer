8023b3cd39257c3b9a2777eac992e5d1e99582cd
pr-olga <frkhorkovaolga@gmail.com>
2021-10-31 15:36:07 +0100

    course-2: server: add basic test to concat

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
@@ -1,4 +1,4 @@
-import { add, divide } from './units';
+import { add, divide, concat } from './units';
 
 import { expect } from 'chai';
 import 'mocha';
@@ -38,3 +38,15 @@ describe('divide', () => {
 // @TODO try creating a new describe block for the "concat" method
 // it should contain an it block for each it statement in the units.ts @TODO.
 // don't forget to import the method ;)
+
+describe('concat', () => {
+  it('should concat olga and udacity', () => {
+    const result = concat('olga','udacity');
+    expect(result).to.equal('olga udacity');
+  });
+
+  it('should throw an error since one param is empty', () => {
+    expect(()=>{ concat('','udacity') }).to.throw('one param is empty')
+  });
+
+})
