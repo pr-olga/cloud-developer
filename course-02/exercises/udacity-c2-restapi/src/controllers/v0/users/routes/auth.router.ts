3ad1e45034ba37f61970ec5f0984188e49ee16ec
pr-olga <frkhorkovaolga@gmail.com>
2022-04-26 12:32:41 +0200

    course-2: security: add bcrypt for hashing and salting

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -12,12 +12,19 @@ const router: Router = Router();
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
     //@TODO Use Bcrypt to Generated Salted Hashed Passwords
-    return "NotYetImplemented"
+    const saltRounds = 10;
+
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(plainTextPassword, salt);
+
+    return hash;
 }
 
 async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {
     //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
-    return true
+    const compare = await bcrypt.compare(plainTextPassword, hash);
+
+    return compare;
 }
 
 function generateJWT(user: User): string {
@@ -31,13 +38,13 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
     // if (!req.headers || !req.headers.authorization){
     //     return res.status(401).send({ message: 'No authorization headers.' });
     // }
-    
+
 
     // const token_bearer = req.headers.authorization.split(' ');
     // if(token_bearer.length != 2){
     //     return res.status(401).send({ message: 'Malformed token.' });
     // }
-    
+
     // const token = token_bearer[1];
 
     // return jwt.verify(token, "hello", (err, decoded) => {
@@ -48,8 +55,8 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
     // });
 }
 
-router.get('/verification', 
-    requireAuth, 
+router.get('/verification',
+    requireAuth,
     async (req: Request, res: Response) => {
         return res.status(200).send({ auth: true, message: 'Authenticated.' });
 });
